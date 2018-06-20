5bf7f845de6c89081cd5ea31daee09b995eba77b
aleksashk <40260846+aleksashk@users.noreply.github.com>
2018-06-20 19:57:28 +0300

    gus_v1.c

---
+++ gus_v1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main()
+
+{
+    float yd;
+    printf("Pozdrawlay!!! Vi GUS i vsvjazi s etim vopros:\n");
+    printf("Kak vi oceniwaete washy stepen ydovolstvija ot 10 do 1?:\n");
+    scanf("%f", &yd);
+    (yd>=5) ? (printf("VSE OK\n")) : (printf("Leti guljat s kocikami\n"));
+    return 0;
+}
