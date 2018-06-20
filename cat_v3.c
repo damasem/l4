0c6fc572c2bc27508fd4a631673f6ed74c59f5ca
aleksashk <40260846+aleksashk@users.noreply.github.com>
2018-06-20 19:39:29 +0300

    cat_v3.c

---
+++ cat_v3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main()
+{
+float m_cat;
+    do
+    {
+        printf("m_cat?\n");
+        scanf("%f",&m_cat);
+            if (m_cat-4<0)
+                {
+                    printf("Kuschaj kocik njam-njam!!!\n");
+                }
+    }
+    while (m_cat-4<0);
+printf("Kocik oshen tolstij!!! Kocik idi gulac!!!\n");
+return 0;
+}
