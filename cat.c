df3e224ff034c43465ce68a34d29082141c79422
aleksashk <40260846+aleksashk@users.noreply.github.com>
2018-06-20 19:32:31 +0300

    cat.c

---
+++ cat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+main()
+{
+float m_cat, m_caty;
+printf("m_cat?\n");
+scanf("%f",&m_cat);
+printf("m_caty?\n");
+scanf("%f",&m_caty);
+    if(m_cat-4<0)
+        {
+            printf("Kocik cat kuchaj njam-njam\n");
+        }
+     else
+        {
+            printf("Kocik cat ect only grass, spat only street!!!!\n");
+        }
+        if(m_caty-4<0)
+        {
+            printf("Kocik caty kuchaj njam-njam\n");
+        }
+     else
+        {
+            printf("Kocik caty ect only grass, spat only street!!!!\n");
+        }
+return 0;
+}
